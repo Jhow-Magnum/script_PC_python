f38c509e9aa1f52bc9c9458083f992ff8c2296e2
jhow-magnum <jhowmagnumweb@outlook.com>
2025-02-25 17:35:45 -0300

    Atualização 1.5

--- diagnostic-tools.py
+++ diagnostic-tools.py
@@ -13,43 +13,43 @@ reset = "\033[0m"
 # Função para limpar a tela
 def limpar_tela():
     if platform.system() == "Windows":
-        os.system("cls")  # Comando para Windows
+        os.system("cls")  
     elif platform.system() == "Linux":
-        os.system("clear")  # Comando para Linux/macOS
+        os.system("clear")  
 
-# Função para verificar a versão do sistema
-def versao_sistema():
-    if platform.system() == "Windows":
-        os.system("wmic os get Caption, Version")  # Corrige o comando
-    elif platform.system() == "Linux":
-        resultado = subprocess.run(["lsb_release", "-d", "-r"], capture_output=True, text=True)
-        linhas = resultado.stdout.splitlines()
-        distro = linhas[0].split(":")[1].strip()
-        version = linhas[1].split(":")[1].strip()
-        print(f"{distro} {version}")
 
 # Função para o submenu de rede
 def submenu_rede():
     while True:
         limpar_tela()
-        print("\n==== Teste em Rede ====")
-        print("[1] IP")
-        print("[2] PING")
-        print("[3] Voltar ao menu principal")
+        print("\n==== Diagnóstico de Rede ====")
+        print("[1] Exibir informações do IP e interfaces de rede")
+        print("[2] Realizar teste de PING")
+        print("[3] Mostrar conexões de rede ativas")
+        print("[4] Verificar conectividade com traceroute")
+        print("[5] Listar usuários que realizaram login anteriormente")
+        print("[6] Identificar o usuário logado no sistema")
+        print("[7] Obter o IP local da máquina")
+        print("[8] Obter o IP de um site remoto")
+        print("[9] Exibir redes cabeadas disponíveis")
+        print("[10] Exibir MAC Address das interfaces de rede")
+        print("[11] Limpar cache de navegação (Chrome e Firefox)")
+        print("[12] Apagar cookies de navegação")
+        print("[13] Voltar ao menu principal")
         opcao_submenu_rede = str(input("\n Escolha a opção desejada: "))
         
-        ## Opção 1 - IPCONFIG
+        ## [1]  Exibir informações do IP e interfaces de rede
         if opcao_submenu_rede == "1":
             if sistema == "Windows":
                 resultado = subprocess.run("ipconfig", shell=True, capture_output=True, text=True)
-            elif sistema == "Linux":  # Aqui inclui também o WSL
+            elif sistema == "Linux":
                 resultado = subprocess.run("ip a", shell=True, capture_output=True, text=True)
             print("Saída:\n", resultado.stdout)
             if resultado.stderr:
                 print(f"Erro:\n{resultado.stderr}")
-            input("Pressione Enter para continuar...")  # Pausa aqui
-       
-        ## Opção 2 - PING
+            input("Pressione Enter para continuar...")  # Pausa aqui       
+        
+        ## [2] Realizar teste de PING
         elif opcao_submenu_rede == "2":
             ip = str(input("Digite o endereço IP desejado exemplo (python.org): ")).strip()
             if sistema == "Windows":
@@ -68,17 +68,141 @@ def submenu_rede():
                 print(f"\nErro ao tentar conectar ao IP ou domínio: {ip}")  # Mensagem de erro
             input("Pressione Enter para continuar...")  # Pausa aqui
 
-        elif opcao_submenu_rede == "3":
+        ## [3] Mostrar conexões de rede ativas
+        elif opcao_submenu_rede == "3": 
+            if sistema == "Windows":
+                resultado = subprocess.run("netstat -an", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("ss -tuln", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+        ## [4] Verificar conectividade com traceroute em tempo real
+        elif opcao_submenu_rede == "4": 
+            ipSite = input("Digite o ip (site) para verificar a conectividade com traceroute:  ")
+            if sistema == "Windows":
+                # Utilizando Popen para Windows
+                processo = subprocess.Popen(["tracert",ipSite], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            elif sistema == "Linux":
+                # Utilizando Popen para Linux
+                 processo = subprocess.Popen(["tracepath", ipSite], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                
+
+            # Exibir a saída em tempo real
+            for linha in processo.stdout:
+                print(linha, end="")  # O end="" impede a inserção de uma nova linha
+            for linha in processo.stderr:
+                print(f"Erro: {linha}", end="")  # Exibe os erros, se houver
+
+            processo.wait()  # Espera o processo terminar antes de continuar
+            input("Pressione Enter para continuar...")
+
+        ## [5] Listar usuários que realizaram login anteriormente
+        elif opcao_submenu_rede == "5": 
+            if sistema == "Windows":
+                resultado = subprocess.run("net user", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("who", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+        ## [6] Identificar o usuário logado no sistema
+        elif opcao_submenu_rede == "6": 
+            if sistema == "Windows":
+                resultado = subprocess.run("echo %USERNAME%", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("whoami", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+        ## [7] Obter o IP público local
+        elif opcao_submenu_rede == "7": 
+            if sistema == "Windows":
+                resultado  = subprocess.run("curl ipecho.net/plain", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("curl ifconfig.me", shell=True, capture_output=True, text=True) 
+            print(f"\nIP público local da máquina: {resultado.stdout}\n ")
+            
+            input("Pressione Enter para continuar...")
+
+        ## [8] Obter o IP de um site remoto
+        elif opcao_submenu_rede == "8": 
+            if sistema == "Windows":
+                resultado = subprocess.run("nslookup www.google.com", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("host www.google.com", shell=True, capture_output=True, text=True) 
+            print("\nResultado:\n", resultado.stdout)
+            input("Pressione Enter para continuar...")
+
+        ## [9] Exibir redes cabeadas disponíveis
+        elif opcao_submenu_rede == "9": 
+            if sistema == "Windows":
+                resultado = subprocess.run("netsh interface show interface", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                print("Para a sua segurança o Linux pedirá acesso ao super usuário...")
+                time.sleep(0.8)
+                resultado = subprocess.run("sudo lshw -class network -short", shell=True, capture_output=True, text=True) 
+            print("\nSaída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+
+        ## [10] Exibir MAC Address das interfaces de rede
+        elif opcao_submenu_rede == "10": 
+            if sistema == "Windows":
+                resultado = subprocess.run("getmac", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("ip link show", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+        ## [11] Limpar cache de navegação (Chrome e Firefox)
+        elif opcao_submenu_rede == "11": 
+            if sistema == "Windows":
+                resultado = subprocess.run("del /q /f %LocalAppData%\\Google\\Chrome\\User Data\\Default\\Cache\\*", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("rm -rf ~/.cache/google-chrome/*", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+        ## [12] Apagar cookies de navegação
+        elif opcao_submenu_rede == "12": 
+            if sistema == "Windows":
+                resultado = subprocess.run("del /q /f %LocalAppData%\\Google\\Chrome\\User Data\\Default\\Cookies", shell=True, capture_output=True, text=True) 
+            elif sistema == "Linux":
+                resultado = subprocess.run("rm -rf ~/.config/google-chrome/*/Cookies", shell=True, capture_output=True, text=True) 
+            print("Saída:\n", resultado.stdout)
+            if resultado.stderr:
+                print(f"Erro:\n{resultado.stderr}")
+            input("Pressione Enter para continuar...")
+
+
+
+        ## [13] Voltar ao menu anterior
+        elif opcao_submenu_rede == "13":
             print("Voltando ao menu principal...")
             time.sleep(1.7)
             limpar_tela()
             return
+        else:
+            input("Opção inválida!\nPressione Enter e tente novamente.")
 
 # Função para o submenu de hardware
 def submenu_hardware():
     while True:
         limpar_tela()
-        print("\n==== Teste de Hardware ====")
+        print("\n==== Informações de Hardware ====")
         print("[1] Teste de disco (HDD / SSD)")
         print("[2] Teste de memória RAM")
         print("[3] Voltar ao menu principal")
@@ -131,39 +255,76 @@ def submenu_hardware():
             time.sleep(1.7)
             limpar_tela()
             return
+        else:
+            input("Opção inválida!\nPressione Enter e tente novamente.")
 
 # Função para o submenu de software
 def submenu_software():
     while True:
         limpar_tela()
-        print("\n==== Submenu - Testes de Software  ====")
-        print("[1] Teste de Software 1")
-        print("[2] Teste de Software 2")
-        print("[3] Voltar ao menu principal")
-
+        print("\n==== Informações de Software ====")
+        print("[1] Pacotes(softwares) instalados ")
+        print("[2] Drivers instalados ")
+        print("[3] Voltar ao menu principal ")
         opcao_submenu_soft = str(input("\n Escolha a opção desejada: "))
+        
+
         if opcao_submenu_soft == "1":
-            print("Opção 1 de Software")
+            if platform.system() == "Windows":
+                print("Carregando...")
+                # Executando o comando e capturando a saída
+                resultado = subprocess.run(["wmic", "product", "get", "name,version"], shell=True, capture_output=True, text=True)
+            
+            elif sistema == "Linux":  
+                resultado = subprocess.run("dpkg --list", shell=True, capture_output=True, text=True)
+
+                 # Verificando se há erro na execução
+            if resultado.stderr:
+                print("Erro na execução do comando:", resultado.stderr)
+                
+            else: # Exibindo a saída (stdout)
+                print("\nResultado da execução do comando:")
+                print(resultado.stdout)
+            
             input("Pressione Enter para continuar...")
+
         elif opcao_submenu_soft == "2":
-            print("Opção 2 de Software")
-            input("Pressione Enter para continuar...")
+            if platform.system() == "Windows":
+                print("Carregando...")
+                
+                resultado = subprocess.run(["driverquery"], shell=True, capture_output=True, text=True)
+            
+            elif sistema == "Linux":  
+                resultado = subprocess.run("lsmod", shell=True, capture_output=True, text=True)
+
+                
+            if resultado.stderr:
+                print("Erro na execução do comando:", resultado.stderr)
+                
+            else:
+                print("\nResultado da execução do comando:")
+                print(resultado.stdout)
+            
+
+            input("Pressione Enter para continuar...")    
         elif opcao_submenu_soft == "3":
             print("Voltando ao menu principal...")
             time.sleep(1.7)
             limpar_tela()
             return
+        else:
+            input("Opção inválida!\nPressione Enter e tente novamente.")
 
 # Identifica o sistema operacional
 sistema = platform.system()     # Detecta automaticamente o sistema operacional
 usuario = os.getenv("USERNAME") if sistema == "Windows" else os.getenv("USER")
 diretorio = os.getcwd()
 
-def get_os_info():
+# Função para obter dados do sistemas operacinal
+def obtem_dados_sistemas():
     sistema = platform.system()
     
     if sistema == "Windows":
-        # Corrige o comando para obter tanto a descrição quanto a versão
         result = subprocess.run(["wmic", "os", "get", "Caption,Version"], capture_output=True, text=True)
         lines = [line.strip() for line in result.stdout.splitlines() if line.strip()]
         if len(lines) >= 2:
@@ -186,12 +347,21 @@ def get_os_info():
 
     return sistema, "Desconhecido", "Desconhecido"
 
+# Função para verificar a versão do sistema
+def versao_sistema():
+    if platform.system() == "Windows":
+        os.system("wmic os get Caption, Version")  # Corrige o comando
+    elif platform.system() == "Linux":
+        resultado = subprocess.run(["lsb_release", "-d", "-r"], capture_output=True, text=True)
+        linhas = resultado.stdout.splitlines()
+        distro = linhas[0].split(":")[1].strip()
+        version = linhas[1].split(":")[1].strip()
+        print(f"{distro} {version}")
 
-
-# Função de MENU principal
+# Função do MENU principal
 def menu_principal():
     limpar_tela()
-    sistema, caption, version = get_os_info()  # Corrige a chamada da função
+    sistema, caption, version = obtem_dados_sistemas()  # Corrige a chamada da função
     while True:
         # Exibe as informações
         print(f"{verde}Usuário logado:{reset} {usuario}")
@@ -207,14 +377,18 @@ def menu_principal():
             
         
 
-        print("\n=============================="
-              +"\n        MENU PRINCIPAL \n"
-              "==============================")
-        print("[1] Rede\n"
-              +"[2] Hardware\n"
-              +"[3] Software\n"
+        
+        print("\n"+"=" * 40)
+        print("      SISTEMA DE DIAGNÓSTICO - MENU      ")
+        print("=" * 40)
+        print("Selecione uma opção:")
+        print("[1] Consultar informações de rede\n"
+              +"[2] Exibir detalhes do hardware\n"
+              +"[3] Listar softwares instalados\n"
+              +"[4] Exportar dados para arquivo CSV\n"
               +"[0] Finalizar Programa")
-        opcao_menu = str(input("\nEscolha uma opção: "))
+        print("=" * 40)
+        opcao_menu = str(input("\nDigite o número da opção desejada: "))
 
         if opcao_menu == "1":
             submenu_rede() # Chama o submenu rede    
